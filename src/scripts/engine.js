da802784274226662ee5325a82ad4eedd5c98753
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-29 14:51:18 -0300

    Enumeração das Cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,34 @@ const state = {
     },
 };
 
+const pathImages = ".src/assets/icons/";
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: ` ${pathImages}dragon.png`,
+        winOf: [1],
+        loseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: ` ${pathImages}magician.png`,
+        winOf: [2],
+        loseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: ` ${pathImages}exodia.png`,
+        winOf: [0],
+        loseOf: [1],
+    },
+];
+
 function init() {
     
 };
