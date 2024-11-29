c2b1e1cb0c53ce06010e8ac7c047994820b917b1
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-29 15:23:15 -0300

    Update Js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,11 @@ const state = {
     },
 };
 
+const playerSide = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}; 
+
 const pathImages = ".src/assets/icons/";
 const cardData = [
     {
@@ -46,8 +51,18 @@ const cardData = [
     },
 ];
 
+async function drawCards(cardNumbers, fieldSide) {
+    for(let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    };
+};
+
 function init() {
-    
+    drawCards(5, playerSide.player1);
+    drawCards(5, playerSide.computer);
 };
 
 init();
